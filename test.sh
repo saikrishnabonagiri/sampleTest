c5cde75db86bf7e41bb7b50eddb2b9dffa5f83db
Saikrishna Bonagiri <saikrishna.bonagiri@gmail.com>
2017-06-12 00:28:56 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ git remote add origin git@github.com:saikrishnabonagiri/sampleTest.git
 git branch -a
 git checkout master
 git checkout -b $HOSTNAME-$today
-#git push origin $HOSTNAME-$today
+git push origin $HOSTNAME-$today
