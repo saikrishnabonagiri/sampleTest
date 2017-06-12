0130deab8fd0f069a3ad5cb283cc4e1733a4b1aa
Saikrishna Bonagiri <saikrishna.bonagiri@gmail.com>
2017-06-12 00:28:23 -0400

    added

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+today=`date '+%Y_%m_%d:%H_%M_%S'`;
+git init
+git remote add origin git@github.com:saikrishnabonagiri/sampleTest.git 
+git branch -a
+git checkout master
+git checkout -b $HOSTNAME-$today
+#git push origin $HOSTNAME-$today
